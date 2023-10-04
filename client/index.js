2d40c23adf1447ae29a2af12347426ed257206f8
Sushmita <sushmitakumari363@gmail.com>
2023-10-04 10:09:00 +0530

    pwa changes

--- client/index.js
+++ client/index.js
@@ -1,6 +1,28 @@
 // Check if service workers are supported
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', {
-    scope: '/',
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js", {
+    scope: "/",
   });
 }
+
+window.subscribe = async function () {
+  if (!("serviceWorker" in navigator)) {
+    return alert("Service worker not supported");
+  }
+
+  const registeration = await navigator.serviceWorker.ready;
+
+  const subscription = await registeration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(PublicKey),
+  });
+
+  console.log(subscription);
+  await fetch("/subscribe", {
+    method: "POST",
+    body: JSON.stringify(subscription),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+};
